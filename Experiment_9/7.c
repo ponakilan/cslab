4f6a21c700af44bd2067921ca897cdab6b71956b
ponakilan <ponakilan25@gmail.com>
2023-01-15 09:17:59 +0530

    Finished Programs

---
+++ Experiment_9/7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{	
+	char str[50], rev[50];
+	char *sptr = str, *rptr = rev;
+	int i = -1;
+	printf("Enter any word : ");
+	scanf("%s", str);
+	while (*sptr) {
+		sptr++;
+		i++;
+	}
+	while (i >= 0)
+	{	sptr--;
+		*rptr = *sptr;
+		rptr++;
+		--i;
+	}
+	*rptr = '\0';
+	printf("Reverse of string is : %s", rev);
+}
